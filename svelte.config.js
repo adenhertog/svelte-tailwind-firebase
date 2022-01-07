d590dc52a524a3479005454d1ae6f86c183d0b45
Andrew den Hertog <andrew.denhertog@gmail.com>
2022-01-08 09:45:21 +1000

    svelete setup

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import firebaseAdapter from 'svelte-adapter-firebase'
+import preprocess from 'svelte-preprocess'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: firebaseAdapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+}
+
+export default config
