644965526c5b63512311e86491e3a1b49a2bfed4
Andrew den Hertog <andrew.denhertog@gmail.com>
2022-01-08 15:13:56 +1000

    derp

--- src/routes/index.spec.ts
+++ src/routes/index.spec.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import { render } from '@testing-library/svelte';
 
-import Index from './Index.svelte';
+import Index from './index.svelte';
 
 test('welcome page renders', () => {
 	const { getByText } = render(Index, { props: { welcome: 'Welcome' } });
